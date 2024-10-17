07edc9b8bd782fbf6560f49c49612a158fa1057e
Paco <119410424+giacomo-folli@users.noreply.github.com>
2024-10-17 19:38:30 +0200

    new modules

---
+++ 13_sync/sync.go
@@ -0,0 +1,25 @@
+package main
+
+import "sync"
+
+type Counter struct {
+	mu    sync.Mutex
+	value int
+}
+
+// NewCounter returns a new Counter.
+func NewCounter() *Counter {
+	return &Counter{}
+}
+
+// Inc the count.
+func (c *Counter) Inc() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.value++
+}
+
+// Value returns the current count.
+func (c *Counter) Value() int {
+	return c.value
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module hello-world
+module github.com/giacomo-folli/hello-go
 
 go 1.22.0
