fffcc296f1cd65cc5cff814f0ba3818bbe3c04e1
Giacomo <giacomo.folli@studenti.unipr.it>
2025-03-13 09:15:04 +0100

    server

--- 19_bloggenerartor/generator.go
+++ 19_bloggenerartor/generator.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"net/http"
 	"os"
 	"strings"
 
@@ -12,18 +13,29 @@ import (
 	blogrenderer "github.com/giacomo-folli/hello-go/18_blogrenderer"
 )
 
-func main() {
-	posts, err := blogposts.NewPostsFromFS(os.DirFS("./posts"))
+const POSTS_DIR = "./posts"
+const STATIC_DIR = "./static"
+
+func StartServer() {
+	http.Handle("/", http.FileServer(http.Dir(STATIC_DIR)))
+	http.ListenAndServe(":8080", nil)
+}
+
+func LoadPosts() (posts []blogposts.Post) {
+	posts, err := blogposts.NewPostsFromFS(os.DirFS(POSTS_DIR))
 	if err != nil {
 		log.Fatalf("failed to load posts: %v\n", err)
 	}
+	return
+}
 
-	if err := os.Mkdir("./public", 0777); os.IsExist(err) {
-		fmt.Println("\"public\" directory already present")
+func ExportPosts(posts []blogposts.Post) {
+	if err := os.Mkdir(STATIC_DIR, 0777); os.IsExist(err) {
+		fmt.Println("\"static\" directory already present")
 	} else if err != nil {
-		log.Fatalf("failed to create \"public\" directory")
+		log.Fatalf("failed to create \"static\" directory")
 	} else {
-		fmt.Println("Created \"public\" directory")
+		fmt.Println("Created \"static\" directory")
 	}
 
 	for _, p := range posts {
@@ -42,7 +54,7 @@ func main() {
 		fileName := fmt.Sprintf("%s.html", post.SanitisedTitle())
 		fmt.Println("Creating file with name:", fileName)
 
-		f, err := os.Create(strings.Join([]string{"public", fileName}, "/"))
+		f, err := os.Create(strings.Join([]string{STATIC_DIR, fileName}, "/"))
 		if err != nil {
 			log.Fatalf("failed to create file for post %s: %v\n", post.Title, err)
 		}
@@ -57,4 +69,12 @@ func main() {
 			log.Fatalf("failed to write post %s to file: %v\n", post.Title, err)
 		}
 	}
+}
+
+func main() {
+	posts := LoadPosts()
+
+	ExportPosts(posts)
+
+	StartServer()
 }
--- go.mod
+++ go.mod
@@ -1,8 +1,15 @@
 module github.com/giacomo-folli/hello-go
 
-go 1.22.0
+go 1.23.6
 
-require github.com/gdamore/tcell v1.4.0
+toolchain go1.24.1
+
+require (
+	github.com/approvals/go-approval-tests v1.2.0
+	github.com/gdamore/tcell v1.4.0
+	github.com/giacomo-folli/hello-go/17_blogposts v0.0.0-20250312193431-91943fa3ea97
+	github.com/gomarkdown/markdown v0.0.0-20250311123330-531bef5e742b
+)
 
 require (
 	github.com/gdamore/encoding v1.0.0 // indirect
